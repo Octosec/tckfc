d7ed8c7a7f617f66bc007b9ff2e749666ce7dd9a
Halit Alptekin <info@halitalptekin.com>
2015-01-07 23:46:25 +0200

    Add multi processing

--- tckfc.py
+++ tckfc.py
@@ -7,6 +7,12 @@ import logging
 import sys
 import commands
 import itertools
+import multiprocessing
+import functools
+import signal
+import time
+
+__version__ = "0.1.0"
 
 # TCKFC log handler file
 TCKFC_LOG_FILE = ".tckfc.log"
@@ -14,7 +20,8 @@ TCKFC_LOG_FILE = ".tckfc.log"
 # Error messages tuple
 ERROR_MESSAGES = ("TrueCrypt File does not exist", "Mount directory does not exist", 
                   "Already mounted", "Key files folder does not exist", 
-                  "Combination value should be minimum from number of key files")
+                  "Combination value should be minimum from number of key files",
+                  "TrueCrypt not installed.")
 
 
 class TCKFCError(Exception):
@@ -43,64 +50,36 @@ class TCKFCError(Exception):
         """
         return "<TCKFCError: {name} >".format(self.name)
 
-class TrueCryptHandler(object):
+def truecrypt_handler(kc, psw, tcf, mp):
     """
     TrueCrypt Handler
-    Main class for handling truecrypt process
+    @psw: truecrypt file password
+    @tcf: truecrypt encrypted file
+    @mp: mount point of truecrypt file   
+    @kc: possible key files 
     """
-    def __init__(self, password, tc_file, mount_point):
-        """
-        Constructor method
-        @password: truecrypt file password
-        @tc_file: truecrypt encrypted file
-        @mount_point: mount point of truecrypt file
-        """
-        # set the arguments
-        self.password = password
-        self.key_files = None
-        self.tc_file = tc_file
-        self.mount_point = mount_point
+    logger = logging.getLogger(__name__)
+    key_files = ",".join(kc)
+    logger.debug("Key files: {0}".format(key_files))
 
-        # Init the logger
-        self.logger = logging.getLogger(__name__)
+    # Init command line string
+    command = "truecrypt -t --non-interactive -p {0} -k {1} {2} {3} &> /dev/null"
+    command = command.format(psw, key_files, tcf, mp)
+    logger.debug("Command: '{0}'".format(command))
 
-        # Check the trucrypt is installed
-        self.__check()
-
-    def run(self):
-        """
-        TrueCrypt process runner
-        """
-        # Create the key files
-        if not self.key_files:
-            raise TCKFCError("Key files are None")
-        self.key_files = ",".join(self.key_files)
-        self.logger.debug("Key files: {0}".format(self.key_files))
-
-        # Init command line string
-        command = "truecrypt -t --non-interactive -p {0} -k {1} {2} {3} &> /dev/null"
-        command = command.format(self.password, self.key_files, self.tc_file, self.mount_point)
-        self.logger.debug("Command: '{0}'".format(command))
-
-        # Execute command
-        result = commands.getstatusoutput(command)
-        self.logger.debug("Command result status: '{0}'".format(result[0]))
-        self.logger.debug("Command result: '{0}'".format(result[1]))
-        # Check the result
-        if not result[0] and not result[1]:
-            self.logger.info("Successfully opened with: '{0}'".format(self.key_files))
-            commands.getstatusoutput("truecrypt -d {0}".format(self.tc_file))
-            self.logger.info("Mount point removed")
-            sys.exit(0)
-        else:
-            self.logger.info("Failed with: '{0}'".format(self.key_files))
+    # Execute command
+    result = commands.getstatusoutput(command)
+    logger.debug("Command result status: '{0}'".format(result[0]))
+    logger.debug("Command result: '{0}'".format(result[1]))
     
-    def __check(self):
-        """
-        Check the truecrypt is installed
-        """
-        if commands.getstatusoutput("which truecrypt")[0]:
-            raise TCKFCError("TrueCrypt not installed")
+    # Check the result
+    if not result[0] and not result[1]:
+        logger.info("Successfully opened with: '{0}'".format(key_files))
+        commands.getstatusoutput("truecrypt -d {0}".format(tcf))
+        logger.info("Mount point removed")
+        sys.exit(0)
+    else:
+        logger.info("Failed with: '{0}'".format(key_files))    
 
 
 class TCKFC(object):
@@ -119,6 +98,8 @@ class TCKFC(object):
         self.password = args.password
         self.mount_point = args.mountpoint
         self.combination = args.combination
+        self.pool = multiprocessing.Pool(multiprocessing.cpu_count(), self.__init_worker
+            )
 
         # Init the logger
         self.logger = logging.getLogger(__name__)
@@ -140,14 +121,24 @@ class TCKFC(object):
             raise TCKFCError(ERROR_MESSAGES[4])   
         self.logger.debug("Key file combinations {0}".format(self.combination))       
         
-        # Init the TrueCrypt handler
-        self.tc_handler = TrueCryptHandler(self.password, self.tc_file, self.mount_point)
+        # Create combinations
+        combinations = itertools.combinations(self.key_files, self.combination)
 
-        # Create all possible combinations and invoke trucrypt with its
-        for c in itertools.combinations(self.key_files, self.combination):
-            self.tc_handler.key_files = c
-            self.logger.debug("Invoke TrueCrypt: {0}, {1}, {2}, {3}".format(self.password, c, self.tc_file, self.mount_point))
-            self.tc_handler.run()
+        # Create partial crack function
+        crack_function = functools.partial(truecrypt_handler, psw=self.password, tcf=self.tc_file, mp=self.mount_point)
+
+        # Crack with multiple cores and wait
+        self.pool.map_async(crack_function, combinations)
+
+        # Handle ctrl-c
+        try:        
+            time.sleep(10)
+        except KeyboardInterrupt:
+            self.pool.terminate()
+            self.pool.join()
+        else:
+            self.pool.close()
+            self.pool.join()                  
 
     def __get_key_files(self):
         """
@@ -172,7 +163,13 @@ class TCKFC(object):
         if os.path.ismount(self.mount_point):
             raise TCKFCError(ERROR_MESSAGES[2])
         if not os.path.isdir(self.key_files_dir):
-            raise TCKFCError(ERROR_MESSAGES[3])        
+            raise TCKFCError(ERROR_MESSAGES[3])
+        if commands.getstatusoutput("which truecrypt")[0]:
+            raise TCKFCError(ERROR_MESSAGES[4])
+
+    def __init_worker(self):
+        signal.signal(signal.SIGINT, signal.SIG_IGN)
+
 
 if __name__ == "__main__":
     # Init the root logger with log format
