474fd3862780c08e6243be6eb7af0365ca0357c7
Halit Alptekin <info@halitalptekin.com>
2015-01-08 00:50:27 +0200

    Update setup file

--- setup.py
+++ setup.py
@@ -4,17 +4,16 @@ except ImportError:
     from distutils.core import setup
 
 config = dict(description='Asynchronously TrueCrypt key file brute force tool',
-			  long_description=open('README.md').read(),
-			  author='Halit Alptekin',
-              url='https://github.com/Octosec/tckfc', 
-              author_email='info@halitalptekin.com', 
-              license='MIT',
-              keywords='truecrypt, password, crack, security, tool', 
-              version='0.3.2', 
-              packages=['tckfc'], 
-              scripts=[], 
-              name='tckfc', 
-              entry_points={
-              'console_scripts': ['tckfc = tckfc.tckfc:main', ]})
+			long_description=open('README.md').read(),
+	      	author='Halit Alptekin',
+            url='https://github.com/Octosec/tckfc', 
+            author_email='info@halitalptekin.com', 
+            license='MIT',
+            keywords='truecrypt, password, crack, security, tool', 
+            version='0.3.2', 
+            packages=['tckfc'], 
+            scripts=[], 
+            name='tckfc', 
+            entry_points={'console_scripts': ['tckfc = tckfc.tckfc:main', ]})
 
 setup(**config)
